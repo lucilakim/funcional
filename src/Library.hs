d9b6b4f62f66512bff10e415df6c48e025373104
Lucila Kim <lukim@frba.utn.edu.ar>
2025-12-16 10:26:55 -0300

    Haskell-Hollidays_ Termina Punto2 (Recuperatorio completo).

--- src/Library.hs
+++ src/Library.hs
@@ -70,9 +70,9 @@ Se pide que evite especialmente la repetición de código.--}
 type PlanTuristico = Persona -> Persona
 -- ● Villa Gesell: depende del mes, si es enero o febrero (mes 1 ó 2) aumenta el nivel de stress en 10,
 -- en el resto del año disminuye el stress en la mitad
-villaGessel :: Number -> PlanTuristico
+villaGessel :: String -> PlanTuristico
 villaGessel mes    
-    | (== 1) mes || (==2) mes = modificarEstres (+10)  
+    | (== "enero") mes || (== "febrero") mes = modificarEstres (+10)  
     | otherwise = modificarEstres (/2) 
 
 -- ● Las Toninas: si el plan es con plata disminuye el nivel de stress de la persona a la mitad, si se va
@@ -97,3 +97,53 @@ puertoMadryn persona = persona { cantAmigos = (+1) . cantAmigos $ persona }
 laAdela :: PlanTuristico
 laAdela = id
 
+{--
+a) Queremos saber si en un conjunto de planes turísticos hay alguno que sea piola para una persona,
+esto implica que su nivel de stress bajaría en caso que vaya.
+
+b) Muestre el ejemplo de cómo invocaría a la función utilizando un ejemplo con planes variados (Villa
+Gesell en enero, Las Toninas con plata, Puerto Madryn y La Adela).
+
+c) Mostrar otro ejemplo de invocación con otros datos, que permita obtener un resultado diferente.
+--}
+
+-- a) Queremos saber si en un conjunto de planes turísticos hay alguno que sea piola para una persona,
+-- esto implica que su nivel de stress bajaría en caso que vaya.
+hayAlgunPlanPiola :: Persona -> [PlanTuristico] -> Bool
+hayAlgunPlanPiola persona planes = 
+    any (< nivelDeEstress persona) 
+        (map (nivelDeEstress . ($ persona)) planes)
+
+
+-- b) Muestre el ejemplo de cómo invocaría a la función utilizando un ejemplo con planes variados (Villa
+-- Gesell en enero, Las Toninas con plata, Puerto Madryn y La Adela).
+planesVariados :: [PlanTuristico]
+planesVariados = [villaGessel "enero", lasToninas True, puertoMadryn, laAdela]
+
+pedro = Persona {
+    nombre = "Pedro",
+    nivelDeEstress = 10,
+    preferencias = ["playa"],
+    cantAmigos = 1000
+} 
+
+-- Ejemplo de invocacion:
+-- hayAlgunPlanPiola :: Persona -> PlanesTuristicos -> Bool
+-- hayAlgunPlanPiola pedro planesVariados
+
+
+-- c) Mostrar otro ejemplo de invocación con otros datos, que permita obtener un resultado diferente.
+
+juana =  Persona {
+    nombre = "Juana",
+    nivelDeEstress = 1000,
+    preferencias = ["gastronomia"],
+    cantAmigos = 5
+}
+
+planesEstresantes :: [PlanTuristico]
+planesEstresantes = [villaGessel "febrero", lasToninas False]
+
+-- Ejemplo de invocacion:
+-- hayAlgunPlanPiola :: Persona -> PlanesTuristicos -> Bool
+-- hayAlgunPlanPiola juana planesEstresantes
